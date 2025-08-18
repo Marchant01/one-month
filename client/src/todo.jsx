d1227d7758b22ac05cc2e8d9688124670eadd5ac
Marchant01 <celvin.marchant@hotmail.com>
2025-08-18 07:22:52 +0200

    created todo.jsx file

--- client/src/todo.jsx
+++ client/src/todo.jsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from 'react'
+
 export default function Todo() {
+  const [Errand,setErrand] = useState([])
   return <main>
-    <h1>Pingas</h1>
+    <h1>Todo:</h1>
+    <ShowErrands/>
   </main>
+
+
+  function ShowErrands() {
+      useEffect(() => {
+        fetch('api/errands')
+          .then((res) => res.json())
+          .then((data) => setErrand(data))
+      }, [])
+    }
+
+    // if !data
+    // return message
+    // return <ul> element
+
+    if(!data) {
+      return <ul>
+        <ul>
+          
+        </ul>
+      </ul>
+    }
 }
